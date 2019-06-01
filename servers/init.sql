48e75f4af016a7864b1aa6ec0dad6d578294fc61
Vojko Drev <vojkodrev@gmail.com>
2019-06-01 12:05:52 +0200

    Create table with init script.

--- servers/init.sql
+++ servers/init.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE rabbitsave;
+
+USE rabbitsave;
+
+CREATE TABLE public.sortsaveline (	
+	marketid int4 NOT NULL,
+	matchid int4 NOT NULL,
+	outcomeid varchar(255) NULL,
+	receivedat int8 NOT NULL,
+	savedat int8 NOT NULL,
+	specifiers varchar(255) NULL
+);
