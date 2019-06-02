9a4ab00e82b9e184f44d6a302c0869d56e579187
Vojko Drev <vojkodrev@gmail.com>
2019-06-02 20:48:53 +0200

    Changed primary key to auto increment

--- servers/init.sql
+++ servers/init.sql
@@ -2,8 +2,10 @@ CREATE DATABASE rabbitsave;
 
 \c rabbitsave;
 
+CREATE SEQUENCE sortsaveline_id_seq;
+
 CREATE TABLE public.sortsaveline (	
-  id int4 NOT NULL PRIMARY KEY,
+  id int4 NOT NULL DEFAULT nextval('sortsaveline_id_seq') PRIMARY KEY,
 	marketid int4 NOT NULL,
 	matchid int4 NOT NULL,
 	outcomeid varchar(255) NULL,
@@ -11,3 +13,5 @@ CREATE TABLE public.sortsaveline (
 	savedat int8 NOT NULL,
 	specifiers varchar(255) NULL
 );
+
+ALTER SEQUENCE sortsaveline_id_seq OWNED BY public.sortsaveline.id;
