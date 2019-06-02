b4ad115d5a77007eeeb50bb4a0cce85365945a99
Vojko Drev <vojkodrev@gmail.com>
2019-06-02 20:36:17 +0200

    Added primary key to table.

--- servers/init.sql
+++ servers/init.sql
@@ -3,6 +3,7 @@ CREATE DATABASE rabbitsave;
 \c rabbitsave;
 
 CREATE TABLE public.sortsaveline (	
+  id int4 NOT NULL PRIMARY KEY,
 	marketid int4 NOT NULL,
 	matchid int4 NOT NULL,
 	outcomeid varchar(255) NULL,
