e2a291a76102e248520e1f306f6d5c6b471fe8b6
Vojko Drev <vojkodrev@gmail.com>
2019-06-05 10:44:09 +0200

    Save data to mongo db.

--- rabbitSaveReadFromQueueAndSaveToDb/src/main/java/com/vojkodrev/rabbitSave/readFromQueueAndSaveToDb/PostgresDbSaver.java
+++ rabbitSaveReadFromQueueAndSaveToDb/src/main/java/com/vojkodrev/rabbitSave/readFromQueueAndSaveToDb/PostgresDbSaver.java
@@ -1,21 +1,24 @@
 package com.vojkodrev.rabbitSave.readFromQueueAndSaveToDb;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import org.apache.log4j.Logger;
+import org.bson.Document;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class PostgresDbSaver implements ObservableSource<List<Line>> {
 
   private final List<Line> list;
 
   final static Logger logger = Logger.getLogger(PostgresDbSaver.class);
-  static Connection connection;
+  static MongoCollection<Document> collection;
 
   public PostgresDbSaver(List<Line> item) {
     this.list = item;
@@ -33,29 +36,24 @@ public class PostgresDbSaver implements ObservableSource<List<Line>> {
         return;
       }
 
-      StringBuilder query = new StringBuilder("INSERT INTO entry (marketid, matchid, outcomeid, receivedat, savedat, specifiers) VALUES ");
-
-      for (int i = 0; i < list.size(); i++) {
-        if (i > 0) {
-          query.append(",");
-        }
-
-        query.append("(?,?,?,?,?,?)");
-      }
-
-      PreparedStatement preparedStatement = connection.prepareStatement(query.toString());
+      List<Document> documents = new ArrayList<>(list.size());
 
       for (int i = 0; i < list.size(); i++) {
         Line ssl = list.get(i);
-        preparedStatement.setInt(1 + 6 * i, ssl.marketId);
-        preparedStatement.setInt(2 + 6 * i, ssl.matchId);
-        preparedStatement.setString(3 + 6 * i, ssl.outcomeId);
-        preparedStatement.setLong(4 + 6 * i, ssl.receivedAt);
-        preparedStatement.setLong(5 + 6 * i, System.currentTimeMillis());
-        preparedStatement.setString(6 + 6 * i, ssl.specifiers);
+
+        Document document = new Document("rabbitSaveEntryId", UUID.randomUUID().toString())
+          .append("marketId", ssl.marketId)
+          .append("matchId", ssl.matchId)
+          .append("outcomeId", ssl.outcomeId)
+          .append("receivedAt", ssl.receivedAt)
+          .append("savedAtMs", System.currentTimeMillis())
+          .append("savedAtNs", System.nanoTime())
+          .append("specifiers", ssl.specifiers);
+
+        documents.add(document);
       }
 
-      preparedStatement.executeUpdate();
+      collection.insertMany(documents);
 
       observer.onNext(list);
       observer.onComplete();
@@ -66,21 +64,24 @@ public class PostgresDbSaver implements ObservableSource<List<Line>> {
 
   }
 
-  private void connect() throws SQLException {
-    if (connection == null) {
-      String username = System.getenv("POSTGRES_USERNAME");
+  private void connect() {
+    if (collection == null) {
+      String mongoDbName = System.getenv("MONGODB_DB_NAME");
+      String mongoDbCollection = System.getenv("MONGODB_COLLECTION");
 
-      String connectionString = "jdbc:postgresql://"
-        + System.getenv("POSTGRES_HOST")
-        + ":" + System.getenv("POSTGRES_PORT")
-        + "/" + System.getenv("POSTGRES_DB_NAME");
+      String connectionString = "mongodb://"
+        + System.getenv("MONGODB_HOST")
+        + ":" + System.getenv("MONGODB_PORT");
 
       logger.info("POSTGRESS CONNECTION STRING: " + connectionString);
-      logger.info("POSTGRESS USERNAME: " + username);
+      logger.info("MONGO DB NAME: " + mongoDbName);
+      logger.info("MONGO DB COLLECTION: " + mongoDbCollection);
 
-      connection = DriverManager.getConnection(connectionString, username, System.getenv("POSTGRES_PASSWORD"));
+      MongoClient mongoClient = MongoClients.create(connectionString);
+      MongoDatabase database = mongoClient.getDatabase(mongoDbName);
+      collection = database.getCollection(mongoDbCollection);
 
-      logger.info("CONNECTED TO POSTGRES");
+      logger.info("CONNECTED TO MONGODB");
     }
   }
 }
