2c2abafc2d0bfb55c72076e44a77b7ace807c841
Vojko Drev <vojkodrev@gmail.com>
2019-06-05 10:19:47 +0200

    Added enable sharding for rabbitsave db

--- servers/mongodb/mongos/init.js
+++ servers/mongodb/mongos/init.js
@@ -3,5 +3,6 @@ sh.addShard( "shard-2-repl-set/mongo-shard-2-1:27018");
 
 db.createCollection("entries");
 
+sh.enableSharding("rabbitsave")
 sh.shardCollection("rabbitsave.entries", { "rabbitSaveEntryId" : "hashed" } );
 
